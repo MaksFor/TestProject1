35eac796aed29544881a73103f7637557221d05d
maksymforbatyuk <maksym.forbatyuk@gmail.com>
2022-08-21 21:34:05 +0300

    test lol

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -2,15 +2,24 @@ namespace TestProject1;
 
 public class Tests
 {
+    private WebDriver driver = new ChromeDriver();
+    
     [SetUp]
     public void Setup()
     {
-        WebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("http://www.google.com");
     }
 
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        for (int i = 0; i < 10; i++)
+        {
+            IWebElement query = driver.FindElement(By.Name("q"));
+            query.SendKeys("Hello, Selenium WebDriver!" + i);
+            query.Submit();
+            Console.WriteLine(driver.Title);
+        }
+        driver.Close();
     }
 }
