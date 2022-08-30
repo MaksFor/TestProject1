aec2f3382428e2679a52164dbd2604566cf1201a
forbatyuk <maksym.forbatyuk@gmail.com>
2022-08-30 11:18:40 +0300

    changed number of Sendkeys

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -2,24 +2,24 @@ namespace TestProject1;
 
 public class Tests
 {
-    private WebDriver driver = new ChromeDriver();
+    private readonly WebDriver _driver = new ChromeDriver();
     
     [SetUp]
     public void Setup()
     {
-        driver.Navigate().GoToUrl("http://www.google.com");
+        _driver.Navigate().GoToUrl("https://www.google.com");
     }
 
     [Test]
     public void Test1()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 5; i++)
         {
-            IWebElement query = driver.FindElement(By.Name("q"));
+            IWebElement query = _driver.FindElement(By.Name("q"));
             query.SendKeys("Hello, Selenium WebDriver!" + i);
             query.Submit();
-            Console.WriteLine(driver.Title);
+            Console.WriteLine(_driver.Title);
         }
-        driver.Close();
+        _driver.Close();
     }
 }
