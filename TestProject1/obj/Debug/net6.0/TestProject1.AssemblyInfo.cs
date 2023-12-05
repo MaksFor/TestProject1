4da3e25932c36b56b39abd0610cf29443d955582
MaksFor <maksym.forbatyuk@gmail.com>
2023-12-05 19:30:44 +0200

    test4 VScode

--- TestProject1/obj/Debug/net6.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net6.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baf786629e3ab7f75292ecc27070c5959100f9e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
